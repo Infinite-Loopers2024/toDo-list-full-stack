a7a7d63e7679bee6fdef1efdcf00e937026a784c
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-10-10 17:01:40 +0200

    Co-authored-by: Mårten Söderlind <marten.soderlind@appliedtechnology.se> Co-authored-by: Love <love.bellinder@gmail.com> Co-authored-by: Adrienn <apozsgai97@gmail.com>

---
+++ db.js
@@ -0,0 +1,16 @@
+const books = [
+  {
+    id: 1,
+    name: "The Great Gatsby",
+  },
+  {
+    id: 2,
+    name: "The Great ",
+  },
+  {
+    id: 3,
+    name: "The",
+  },
+];
+
+module.exports = { books };
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import http from "http";
 import fs from "fs";
 import path from "path";
 
-const port = 8080;
+let todos = ["Do the dishes.", "Clean the bathroom.", "Walk the dog."];
+
+const port = 3000;
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     const filePath = path.join(__dirname, "index.html");
@@ -14,6 +16,30 @@ const server = http.createServer((req, res) => {
       res.writeHead(200);
       res.end(content);
     });
+  } else if (req.url === "/add-todo" && req.method === "POST") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+      console.log(body);
+    });
+
+    req.on("end", () => {
+      const parsedBody = new URLSearchParams(body);
+      const newTodo = parsedBody.get("Todo");
+
+      if (newTodo) {
+        todos.push(newTodo);
+        res.writeHead(302, { Location: "/" });
+        res.end();
+      } else {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Bad Request");
+      }
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Not Found");
   }
 });
 
